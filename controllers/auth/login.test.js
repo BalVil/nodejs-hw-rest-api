6ea10c9fb8dd0136f8f4386e7964015514b4bba4
BalVil <balutavili@gmail.com>
2022-10-18 02:39:21 +0300

    added comments, returned previous PORT

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -6,11 +6,10 @@ const app = require("../../app");
 const { User } = require("../../models/userModel");
 
 const { DB_TEST_HOST, PORT = 3000 } = process.env;
-// const login = require("./login");
 
 describe("test auth routes", () => {
   let server;
-  beforeAll(() => (server = app.listen(8083)));
+  beforeAll(() => (server = app.listen(PORT)));
   afterAll(() => server.close());
 
   beforeEach((done) => {
@@ -27,21 +26,26 @@ describe("test auth routes", () => {
       email: "test@gmail.com",
       password: "1234567",
     };
+
     const response = await request(app)
       .post("/api/users/login")
       .send(loginUser);
 
+    // the response must have status code 200
     expect(response.statusCode).toBe(200);
 
+    // the response should return a token and a user object
     const { body } = response;
     expect(body.token).toBeTruthy();
     expect(body.user).toBeTruthy();
 
+    // the user object must have 2 fields: email and subscription with data type String
     expect(body.user).toEqual({
       email: expect.any(String),
       subscription: expect.any(String),
     });
 
+    // verification of token compliance
     const { token } = await User.findOne({ email: loginUser.email });
     expect(body.token).toBe(token);
   });
